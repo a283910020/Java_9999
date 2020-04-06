9f0821446f2a7633fcb0780ff4674b1c8c388350
chenzhuo <a283910020@gmail.com>
2020-04-06 17:01:12 +1000

    9999 update

--- javabase/chapter8/guessdemo/Scanner.java
+++
@@ -1,8 +0,0 @@
-package com.xuetang9.javabase.chapter8.guessdemo;
-
-public class Scanner {
-	public static void main(String[] args) {
-		java.util.Scanner input = new java.util.Scanner(System.in);
-		
-	}
-}
