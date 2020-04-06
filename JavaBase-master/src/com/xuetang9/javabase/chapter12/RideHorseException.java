a725ec6db781be43b2acf701542bb12001259416
chenzhuo <a283910020@gmail.com>
2020-04-06 16:43:56 +1000

    9999 source file update

--- JavaBase-master/src/com/xuetang9/javabase/chapter12/RideHorseException.java
+++
@@ -1,7 +0,0 @@
-package com.xuetang9.javabase.chapter12;
-
-public class RideHorseException extends Exception{
-	public RideHorseException(int health){
-		super("当前健康值为" + health + "，骑马失败！");
-	}
-}
--- JavaBase-master/src/com/xuetang9/javabase/chapter13/FileDemo1.java
+++
@@ -1,6 +0,0 @@
-package com.xuetang9.javabase.chapter13;
-public class FileDemo1{
-	public static void main(String[] args){
-		System.out.println("HelloWorld!");
-	}
-}
