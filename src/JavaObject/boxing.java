85217cade8130aa43684566b4b5381bb6000bbd2
chenzhuo <a283910020@gmail.com>
2020-04-12 21:47:00 +1000

    9999 update

---
+++ src/JavaObject/boxing.java
@@ -0,0 +1,19 @@
+package JavaObject;
+
+public class boxing {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+
+        Integer iNum1 = new Integer(num1);
+        Integer iNum2 = num1;
+
+        String num2 = iNum2.toString();
+        int num3 = iNum2.intValue();
+        System.out.println(num2);
+        System.out.println(num3);
+
+        System.out.println(num2.compareTo(String.valueOf(num3)));
+    }
+
+}
